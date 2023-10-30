ab2f527aa5bb170ed7762aadd5c16426357b1524
HiChen85 <hccc_zhang@outlook.com>
2023-10-30 21:10:03 +0800

    [hccc] feat: add demo generics code

--- src/main.go
+++ src/main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println(add1(1, 2))
+	fmt.Println(add2(1.2, 2.3))
+	fmt.Println(add(1, 9))
+	fmt.Println(add(1.1, 8.9))
+}
+
+func add1(a, b int) int {
+	return a + b
+}
+
+func add2(x, y float64) float64 {
+	return x + y
+}
+
+type addable interface {
+	int | int64 | float64
+}
+
+func add[T addable](x, y T) T {
+	return x + y
 }
